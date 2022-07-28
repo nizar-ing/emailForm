6f0428e47deed199db569453ce8dc2ba0f2a17d7
Nizar ILAHI <nizar.ilahi@gmail.com>
2022-07-28 14:45:47 +0100

    Template Forms driven for a teaching purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emailForm';
+  email: string = '';
+
+  constructor(){}
+
+  onSubmit(): void {
+    console.log(this.email);
+  }
 }
